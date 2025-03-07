64921eaa29863885cbdb1c37db66f830761dac09
yuu2430 <vaishalis2430@gmail.com>
2025-03-07 16:02:33 +0530

    Update codechangenewnew.py

--- codechangenewnew.py
+++ codechangenewnew.py
@@ -108,7 +108,7 @@ if st.button("Submit Guess", key="submit_guess"):
     if correct_location and correct_age and correct_gender:
         st.success(f"\U0001F389 Correct! You've solved the case. Reward: You win a sweet treat! yay!")
     else:
-        st.session_state.attempts -= 1
+        st.session_state.attempts -= 2
         feedback = []
         if not correct_location:
             feedback.append("The location probability suggests another area...")
